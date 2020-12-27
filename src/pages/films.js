69d0b66b2b8217870db79b389fa57d9f130ac739
JorisDenissen <joris.denissen@mail.be>
2020-12-27 16:05:57 +0100

    Film template
    Created the template that will be used to display all the data of a specific film

---
+++ src/pages/films.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+import SEO from '../components/Seo'
+import { Wrapper, Image, BottomEdgeUp, BottomEdgeDown, Film} from './pageStyles/pageStyles'
+import { COLORS } from '../constants'
+
+const FilmsPage = () => {
+    const {
+        wpcontent:{
+            page:{
+                filmsMeta:{
+                    filmsPageDescription,
+                    filmsPageBannerPhoto
+                }
+            },
+            films:{
+                edges: films
+            }
+        }
+    } = useStaticQuery(graphql`
+        query {
+            wpcontent {
+                page(id: "films", idType: URI) {
+                    filmsMeta {
+                        filmsPageDescription
+                        filmsPageBannerPhoto {
+                            sourceUrl
+                            imageFile{
+                                childImageSharp{
+                                    fluid(quality: 100){
+                                        ...GatsbyImageSharpFluid_withWebp
+                                    }
+                                }
+                            }
+                            altText
+                        }
+                    }
+                }
+                films {
+                    edges {
+                        node {
+                            film {
+                                title
+                                poster {
+                                    sourceUrl
+                                    imageFile{
+                                        childImageSharp{
+                                            fluid(quality: 100){
+                                                ...GatsbyImageSharpFluid_withWebp
+                                            }
+                                        }
+                                    }
+                                    altText
+                                }
+                                director
+                            }
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+    return(
+        <Layout>
+            <SEO title="Films"/>
+            <Wrapper filmsColor={COLORS.BLACK} descriptionColor={COLORS.SECONDARY}>
+                <div className="banner">
+                    <Image fluid={filmsPageBannerPhoto.imageFile.childImageSharp.fluid} alt={filmsPageBannerPhoto.altText}/>
+                    <BottomEdgeDown color={COLORS.SECONDARY}/>
+                </div>
+                <div className="description">
+                    <h2>Onze film collectie!</h2>
+                    <p>{filmsPageDescription}</p>
+                    <BottomEdgeUp color={COLORS.BLACK}/>
+                </div>
+                <div className="films">
+                   <div className="film-items">
+                       {films.map(({node: {film, slug}}, i) => (
+                           <Film to={`/${slug}`} key={i}>
+                                <Image fluid={film.poster.imageFile.childImageSharp.fluid} alt={film.poster.altText}/>
+                                <div className="film-info">
+                                    <p>{film.title}</p>
+                                    <p>{film.director}</p>
+                                </div>
+                           </Film>
+                       ))}
+                   </div>
+                </div>
+            </Wrapper>
+        </Layout>
+    )
+}
+
+export default FilmsPage
